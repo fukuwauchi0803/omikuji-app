83575c8d9bfe125f6fbf895a656c6ef3659fb539
原田和明 <haradakazuaki@ChazMBA.local>
2025-08-12 11:29:46 +0900

    fix the code of app.py to show the enviroment name.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import os, random
 from flask import Flask
-import random
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def omikuji():
+    env = os.getenv("APP_ENV", "local")
     fortunes = ["大吉", "中吉", "小吉", "凶"]
-    return f"<h1>今日の運勢: {random.choice(fortunes)}</h1>"
+    return f"<h1>[{env}] 今日の運勢: {random.choice(fortunes)}</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
